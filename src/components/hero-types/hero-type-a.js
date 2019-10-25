cb6bba9f2073faf149d3fffc62dba59c0fc89b42
Richard McSharry <rmcsharry@gmail.com>
2019-10-25 14:38:29 +0200

    Changes hero type a back to func comp

--- src/components/hero-types/hero-type-a.js
+++ src/components/hero-types/hero-type-a.js
@@ -7,52 +7,35 @@ import ScrollSign from '../scroll-sign/scroll-sign'
 import styles from './hero-type-a.module.scss'
 import Typing from 'react-typing-animation';
 
-class HeroTypeA extends React.Component {
-  constructor(props) {
-    super(props);
-    //any initialization here or in other react class methods
-  }
+const HeroTypeA = (props) => {
+  return (
+    <div>
+      <Img className={styles.hero_image} alt={props.heroImageAltText} fluid={props.heroImage.fluid} />
 
-  handleScroll = e => {
-    console.log('SCROLL****')
-    let element = e.target
-    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-      console.log('SCROLL****')
-      // do something at end of scroll
-    }
-  }
-  
-  render() {
-    const props = this.props
-    return (
-      <div onScroll={this.handleScroll}>
-        <Img className={styles.hero_image} alt={props.heroImageAltText} fluid={props.heroImage.fluid} />
-
-        <div className={styles.hero_title}>
-          <div className={`${styles.title_text} is-size-3-desktop is-size-4-tablet is-size-6-mobile has-text-weight-bold`}>
-            <Typing>
-              {documentToReactComponents(props.title)}
-            </Typing>
-          </div>
+      <div className={styles.hero_title}>
+        <div className={`${styles.title_text} is-size-3-desktop is-size-4-tablet is-size-6-mobile has-text-weight-bold`}>
+          <Typing>
+            {documentToReactComponents(props.title)}
+          </Typing>
         </div>
-        <div className={styles.hero_sub_title}>
-          <div className={`${styles.title_text} is-size-3-desktop is-size-4-tablet is-size-6-mobile has-text-weight-bold`}>
-            <Typing>
-              <Typing.Delay ms={2500} />
-              {documentToReactComponents(props.subTitle)}
-            </Typing>
-          </div>
-        </div>
-        {/* CTA button */}
-        <div className={styles.hero_cta}>
-          <button className="button is-danger has-text-weight-bold">
-            REGISTER YOUR INTEREST
-        </button>
-        </div>
-        <ScrollSign />
       </div>
-    )
-  }
+      <div className={styles.hero_sub_title}>
+        <div className={`${styles.title_text} is-size-3-desktop is-size-4-tablet is-size-6-mobile has-text-weight-bold`}>
+          <Typing>
+            <Typing.Delay ms={2500} />
+            {documentToReactComponents(props.subTitle)}
+          </Typing>
+        </div>
+      </div>
+      {/* CTA button */}
+      <div className={styles.hero_cta}>
+        <button className="button is-danger has-text-weight-bold">
+          REGISTER YOUR INTEREST
+      </button>
+      </div>
+      <ScrollSign />
+    </div>
+  )
 }
 
 export default HeroTypeA
